da3e00532d441beac510d451dff07f7ec6e43597
Karol Cissewski <Karol.Cissewski@itechart-group.com>
2022-12-05 08:33:01 +0100

    Add Foreign Function & Memory API simple example

---
+++ main/src/main/java/ForeignFunctionAndMemoryAPISandbox.java
@@ -0,0 +1,28 @@
+import java.lang.foreign.*;
+import java.lang.invoke.MethodHandle;
+
+import static java.lang.foreign.ValueLayout.ADDRESS;
+import static java.lang.foreign.ValueLayout.JAVA_LONG;
+
+public class ForeignFunctionAndMemoryAPISandbox implements Sandbox {
+    @Override
+    public void play() {
+        SymbolLookup stdlib = Linker.nativeLinker().defaultLookup();
+
+        MethodHandle strlen = Linker.nativeLinker().downcallHandle(
+                stdlib.lookup("strlen").orElseThrow(),
+                FunctionDescriptor.of(JAVA_LONG, ADDRESS)
+        );
+
+        MemorySegment str = SegmentAllocator.implicitAllocator().allocateUtf8String("Hello ITechArt!");
+
+        long len;
+        try {
+            len = (long) strlen.invoke(str);
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.printf("len = %d%n", len);
+    }
+}
--- main/src/main/java/Main.java
+++ main/src/main/java/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
         Sandbox sandbox;
 
-        sandbox = new VirtualThreadsSandbox();
+//        sandbox = new VirtualThreadsSandbox();
+        sandbox = new ForeignFunctionAndMemoryAPISandbox();
 
         sandbox.play();
     }
